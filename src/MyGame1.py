d17c858c9bb30b3985215b3b20c339b031a3464f
Will Pimm <willpimm@live.com>
2018-09-17 12:01:47 +0100

    Changes

--- src/MyGame1.py
+++ src/MyGame1.py
@@ -1,93 +1,178 @@
 import pygame,time
+from enum import Enum
+
+class Setup:
+    def __init__(self,width,height):
+        pygame.init()
+        self.player = Player(480,220,10,20)
+        self.users = pygame.sprite.Group()
+        self.users.add(self.player)
+        self.width = width
+        self.height = height
+        pygame.display.set_caption("MyGame")
+        self.running = True
+        self.play()
+        self.screen = Setup(1000,500)
+
+    def play(self):
+        print("3")
+        while self.running:
+            self.player.press()
+            self.draw()
+
+
+    def draw(self):
+        self.screen.fill((0,0,0,0))
+        self.users.draw(screen)
+        self.users.update()
+        self.screen.flip()
+
 
 class Entity(): # Parent class
-    def __init__(self,x,y,width,height,health):
+    def __init__(self,x,y,width,height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.health = health
+
+        #self.health = health
 
 class Zombie(Entity): # sub-class to Entity
     def __init__(self,x,y,width,height,health):
         super().__init__(x,y,width,height,health)
 
-    def move():
-        if you.x > zombie.x:
+    def draw_zombie(self):
+        pass
+
+    def move(self):
+        if player.x > zombie.x:
             zombie.x+=1
         else:
-            zombie.x-=1
-        if you.y > zombie.y:
+            player.x-=1
+        if player.y > zombie.y:
             zombie.y+=1
         else:
             zombie.y-=1
 
-class Player(Entity): # sub-class to Entity
-    def __init__(self,x,y,width,height,health):
-        super().__init__(x,y,width,height,health)
+class Direction(Enum):
+    N = (0,-1)
+    S = (0,1)
+    E = (1,0)
+    W = (-1,0)
+    NE = (-0.5,0.5)
+    NW = (-0.5,-0.5)
+    SE = (0.5,0.5)
+    SW = (0.5,-0.5)
+
+# The following class is a subclass to entity which creates the player object
+class Player(Entity, pygame.sprite.Sprite): # sub-class to Entity
+
+    def __init__(self,x,y,width,height):
+        super().__init__(x,y,width,height)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill((153,50,204))
+        self.rect = self.image.get_rect()
+        self.vel_x = 0
+        self.vel_y = 0
         self.up = False
         self.down = False
         self.left = False
         self.right = False
+        self.press()
 
-    def inputt():
-        running = True
-        while running:
-            btn = pygame.key.get_pressed()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            if btn[pygame.K_LEFT] and you.x>0:
-                you.x -=0.126
-                you.left = True
-            if btn[pygame.K_RIGHT] and you.x <960:
-                you.x+=0.125
-                you.right = True
-            if btn[pygame.K_UP] and you.y>0:
-                you.y -=0.125 # Because co-ordinates have origin top left in python
-                you.up = True
-            if btn[pygame.K_DOWN] and you.y<440:
-                you.y+=0.125
-                you.down = True
-            if btn[pygame.K_SPACE]:
-                Bullet.direction()
-            Screen.draw()
+    def press(self):
+        self.velx = 0
+        btn = pygame.key.get_pressed()
+        if btn[pygame.K_LEFT]:
+            self.vel_x = -5
+        if btn[pygame.K_RIGHT]:
+            self.vel_x = 5
+
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+
+    def fire(self):
+        raise NotImplementedError
+"""
+    def draw_player(self):
+        pygame.display.set_mode((1000,500)).fill(pygame.Color(0,0,0,0))
+        self.rect = self.image.get_rect()
+        pygame.display.flip()
+
+
+
+    def movex(self):
+        if self.left:
+            player.x-= 5
+            self.draw_player()
+            release()
+#        if not self.left:
+
+        else:
+            print("3")
+            self.x +=5
+            self.draw_player()
+
+
+    def movey(self):
+        if self.up:
+            self.y-=5
+        if self.down:
+            self.y+=5
+        self.draw_player()
+"""
 
 class Bullet(Player):
     def __init__(self,up,down,left,right,size):
+        #super().__init__()
         self.size = size
-        self.x = x
 
-    def direction(self):
-        if you.up == True:
-            print("hi")
-        if you.down == True:
-            bullet.direction == down
-        if you.left == True:
-            bullet.direction == left
-        if you.right == True:
-            bullet.direction == right
+    def draw_bullet(self):
+        pass
+        #raise NotImplementedError
+        #pygame.draw.circle(pygame.display.set_mode((screen.width,screen.height)),[105,105,105],(bullet.x,bullet.y), 8)
 
-class Screen():
-    def __init__(self):
-        self.width = 1000
-        self.height = 500
-
-    def draw():
-        board.fill(pygame.Color(0,0,0,0))
-        pygame.draw.rect(board,[153,50,204],[you.x,you.y,you.width,you.height])
-        pygame.draw.circle(board,[105,105,105],(bullet.x,bullet.y), 8)
-        pygame.display.flip()
-
-    def display():
+#This class will...
+class Board():
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
         pygame.display.set_caption("MyGame")
-        Player.inputt()
 
 
+# event is a FIFO queue. Empties event queue and handles quit event. get() removes the first event in the queue
+"""
+class Game():
+        def __init__():
+
+
+def press():
+    vector = pygame.math.Vector2
+    btn = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        if event.type ==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.left = True
+                player.movex()
+            if event.key == pygame.K_RIGHT:
+                player.right = True
+                player.movex()
+            if event.key == pygame.K_UP:
+                player.up = True
+                player.movey()
+            if event.key == pygame.K_DOWN:
+                player.down = True
+                player.movey()
+
+#def release():
+#    if btn[pygame.K_LEFT]:
+"""
 def __main__():
-    pygame.init()
-
-    board = pygame.display.set_mode((1000,500))
-    you = Player(480,220,40,60,100,10)
-    Screen.display()
-    pygame.quit()
+    global screen
+    screen = Setup(1000,500)
+    screen.draw()
+    Setup()
--- tests/test_MyGame1.py
+++ tests/test_MyGame1.py
@@ -1,14 +1,50 @@
 import MyGame1
 # A module is created to test whether the display board is of correct width
 # screen is an instance of Screen which we can use to test variable values.
-def test_displayboardwidth():
-    screen = MyGame1.Screen()
+
+def test_display_board_width():
+    screen = MyGame1.Board(1000,500)
     assert screen.width == 1000,"Screen was incorrect width."
 
-def test_displayboardheight():
-    screen = MyGame1.Screen()
+def test_display_board_height():
+    screen = MyGame1.Board(1000,500)
     assert screen.height == 500,"Screen was incorrect height."
 
-def test_constructZombie():
+def test_construct_Player():
+    player = MyGame1.Player(480,220,10,20)
+    assert player, "player not constructed"
+
+def test_player_width():
+    player = MyGame1.Player(480,220,10,20)
+    assert player.width == 10,"Player incorrect width"
+
+def test_player_height():
+    player = MyGame1.Player(480,220,10,20)
+    assert player.height == 20,"Player inccorect height"
+
+def test_player_stays_in_boundries_left():
+    player = MyGame1.Player(0,0,10,20)
+    player.move_left()
+    assert player.x >=0 ,"Player out of bounds"
+
+def test_player_stays_in_boundries_right():
+    player = MyGame1.Player(1000,490,10,20)
+    player.move_right()
+    assert player.x<1001,"Player out of bounds"
+
+def test_player_stays_in_boundries_up():
+    player = MyGame1.Player(0,0,10,20)
+    player.move_up()
+    assert player.y>-1,"Player out of bounds"
+
+def test_player_stays_in_boundries_down():
+    player = MyGame1.Player(0,500,10,20)
+    player.move_down()
+    assert player.y< 501,"player out of bounds"
+
+"""
+def test_construct_Zombie():
     zombie = MyGame1.Zombie(1, 1, 1, 1, 1)
     assert zombie,"zombie not constructed"
+
+"""
