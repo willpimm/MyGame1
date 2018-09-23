f61916613b1535cea1167eff2ace3a4257d86d24
Will Pimm <willpimm@live.com>
2018-09-23 18:24:14 +0100

    Bullet class created. Player can now shoot bullets. Bullets appear to be going in random directions and are not circular.

--- src/MyGame1.py
+++ src/MyGame1.py
@@ -1,4 +1,4 @@
-import pygame,time, random
+import pygame,time, random, pygame.gfxdraw
 
 class Setup:
     def __init__(self,width,height):
@@ -7,13 +7,15 @@ class Setup:
         self.users = pygame.sprite.Group()
         self.users.add(self.player)
         self.all_enemies = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
         self.width = width
         self.height = height
         self.display = pygame.display.set_mode((1000,500))
         pygame.display.set_caption("MyGame")
+        self.safe = True
         self.running = True
 
-        for z in range(5):
+        for z in range(10):
             self.all_enemies.add(Zombie())
 
     def play(self):
@@ -23,12 +25,25 @@ class Setup:
             self.draw()
             self.player.events()
 
+            for bullet in self.bullets.sprites():
+                bullet.move()
+
             for zombie in self.all_enemies.sprites():
                 zombie.move()
 
+                """
+                while self.safe:
+                    collision = pygame.sprite.spritecollide(self.player,self.all_enemies,False)
+                    if collision:
+                        self.safe = False
+                    else:
+                        self.safe = True
+                        #zombie.rect.x = screen.player.rect.
+"""
     def draw(self):
         self.display.fill((0,0,0,0))
         self.users.draw(self.display)
+        self.bullets.draw(self.display)
         self.all_enemies.draw(self.display)
         pygame.display.flip()
 
@@ -41,9 +56,8 @@ class Entity: # Parent class
 
         #self.health = health
 
-class Zombie(pygame.sprite.Sprite): # sub-class to Entity
+class Zombie(pygame.sprite.Sprite):
     def __init__(self):
-        #super().__init__(x,y,width,height)
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10,20))
         self.image.fill((124,252,0))
@@ -57,7 +71,6 @@ class Zombie(pygame.sprite.Sprite): # sub-class to Entity
             self.rect.x+=1
         else:
             self.rect.x-=1
-
         if screen.player.rect.y > self.rect.y:
             self.rect.y+=1
         else:
@@ -66,7 +79,7 @@ class Zombie(pygame.sprite.Sprite): # sub-class to Entity
 # The following class is a subclass to entity which creates the player object
 class Player(Entity, pygame.sprite.Sprite): # sub-class to Entity
     def __init__(self,x,y,width,height):
-        super().__init__(x,y,width,height)
+        #super().__init__(x,y,width,height)
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10,20))
         self.image.fill((153,50,204))
@@ -92,18 +105,38 @@ class Player(Entity, pygame.sprite.Sprite): # sub-class to Entity
             self.vel_y -= 5
         if btn[pygame.K_DOWN] and self.rect.y<480:
             self.vel_y += 5
+        if btn[pygame.K_SPACE]:
+            self.fire()
 
         self.rect.x += self.vel_x
         self.rect.y += self.vel_y
 
     def fire(self):
-        raise NotImplementedError
+        screen.bullets.add(Bullet(screen.player.rect.x,screen.player.rect.y))
+
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+
+class Bullet(Entity,pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        #super().__init__(x,y,width,height)
+        self.image = pygame.Surface((10,10),pygame.SRCALPHA)
+        pygame.gfxdraw.filled_circle(self.image, 10, 10, 14, (153,50,204))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_x = 5
+        self.vel_y = 5
+
+    def move(self):
+        self.rect.x += self.vel_y
+        self.rect.y += self.vel_x
+
 
 def __main__():
     global screen
